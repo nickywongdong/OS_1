1d88f876a88be1d327206d94240db2f863469243
wongnich <wongnich@Nicholass-MacBook-Pro.local>
2016-11-15 22:40:16 -0800

    step 1
    receive input in an infinite loop
    exit on status change

--- assignment-3/smallsh.c
+++ assignment-3/smallsh.c
@@ -4,8 +4,6 @@
  smallsh.c
  */
 
-
-
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,9 +11,28 @@
 #include <string.h>
 
 
+void parseInput(char input[2048]){
+  
+
+}
+
+void smallsh(){
+  int status=-5;
+  char input[2048];
+  while(status){
+    memset(input, '\0', 2048);
+    getline(&input, stdin);
+    parseInput(input);
+    execCommand();
+  }
+
+
+
+}
 
 int main(){
 
-    
+  smallsh();
+
     return 0;
 }
