6bb76e1579e173a3cbdb0c0f9ee6cde469f51353
wongnich <wongnich@gmail.com>
2016-11-15 20:46:57 -0800

    Add files via upload

---
+++ smallsh.c
@@ -0,0 +1,21 @@
+/*Nick Wong
+ Tuesday, November 15th
+ OS-Class assign3
+ smallsh.c
+ */
+
+
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(){
+
+    
+    return 0;
+}
