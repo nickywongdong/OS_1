661ff3c88df3a45893e8e7d3037b8ae39d8fcec1
wongnich <wongnich@Nicholass-MacBook-Pro.local>
2016-11-22 23:27:35 -0800

    finished
    included 2 rand ints and product

--- assignment-4/mypython.py
+++ assignment-4/mypython.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from random import randint      #used to generate a random integer
 
+
+#Create empty string
 myNumbers = ''
 
 #opening the 3 files and naming them file(i).txt
@@ -17,13 +19,20 @@ for j in range(3):
    myFiles[j].close()
 del myFiles[:]
 
-#re-open files as read only
+#re-open files as read only to print out contents of files
 myFiles = [open('file'+str(i)+'.txt', 'r') for i in range(3)]
 for i in range(3):
    print "Contents of ", myFiles[i].name + ": ", myFiles[i].read()
-#myString = myFiles[0].read()
-#print "contents of string: ", myString
 
 #close the file, again
 for j in range(3):
    myFiles[j].close()
+
+#Generate 2 random ints
+randInt1=randint(1, 42)
+randInt2=randint(1, 42)
+
+#printing out 2 random integers, and then product
+print "Random Int 1: ", randInt1
+print "Random Int 2: ", randInt2
+print "Product of Int 1, and Int 2: ", randInt1*randInt2
