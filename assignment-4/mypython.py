bdd02e57f59aa4681651695573f17fd030e01ef3
wongnich <wongnich@Nicholass-MacBook-Pro.local>
2016-11-22 23:34:18 -0800

    variables

--- assignment-4/mypython.py
+++ assignment-4/mypython.py
@@ -5,18 +5,18 @@ from random import randint      #used to generate a random integer
 #Create empty string
 myNumbers = ''
 
-#opening the 3 files and naming them file(i).txt
+#opening the 3 files and naming them file(i).txt as write only
 myFiles = [open('file'+str(i)+'.txt', 'w') for i in range(3)]
-for x in range(3):
-   for y in range(10):                      #storing 10 rand characters
+for i in range(3):
+   for j in range(10):                      #storing 10 rand characters
       myNumbers = myNumbers+(chr(randint(97, 122)))
    myNumbers=myNumbers+'\n'                 #append a newline char last
-   myFiles[x].write(myNumbers)              #write string to file
+   myFiles[i].write(myNumbers)              #write string to file
    myNumbers=''                             #reset string contents
 
 #close the file
-for j in range(3):
-   myFiles[j].close()
+for i in range(3):
+   myFiles[i].close()
 del myFiles[:]
 
 #re-open files as read only to print out contents of files
@@ -25,8 +25,8 @@ for i in range(3):
    print "Contents of ", myFiles[i].name + ": ", myFiles[i].read()
 
 #close the file, again
-for j in range(3):
-   myFiles[j].close()
+for i in range(3):
+   myFiles[i].close()
 
 #Generate 2 random ints
 randInt1=randint(1, 42)
