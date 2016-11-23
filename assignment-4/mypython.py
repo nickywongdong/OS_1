90ee318dcd352c115a993ee712734aa34af74a43
wongnich <wongnich@Nicholass-MacBook-Pro.local>
2016-11-22 20:20:23 -0800

    removed list
    replaced list with a string. Works well, doesn’t print out [x, x, x]
    
    write works well (can cat into .txt) but read doesn’t work?

--- assignment-4/mypython.py
+++ assignment-4/mypython.py
@@ -3,23 +3,30 @@ from random import randint      #used to generate a random integer
 
 
 
-myNumbers = []
+myNumbers = ''
 #for i in range(10)
-#   myNumbers.append(chr(randint(97, 122)))	#97-122, lowercase ascii's
+#   myNumbers.append(chr(randint(97, 122)))  #97-122, lowercase ascii's
 
 #opening the 3 files and naming them file(i).txt
 myFiles = [open('file'+str(i)+'.txt', 'wb+') for i in range(3)]
 for x in range(3):
-   for y in range(10):
-      if y==10:	break
-      print "y: ", y
-      myNumbers.append(chr(randint(97, 122)))
-      print myNumbers[0:y]
-   print "i'm outside of y loop\n"
+   for y in range(10):                          #storing 10 rand characters
+      #if y==10:   break
+      #print "y: ", y
+      myNumbers = myNumbers+(chr(randint(97, 122)))
+   print myNumbers[:]
+   myNumbers=myNumbers+'\n'                 #append a newline char last
+   myFiles[x].write(myNumbers)               #write contents of list to file
+   myNumbers=''                             #clear contents of the list
+   #rint "i'm outside of y loop\n"
 
+#testing
+for i in myFiles:
+   print "Closed or not: ", i.closed
+   print "File has: ", i.read()
 #print "file: ", myFiles[0].name, myFiles[0].mode
-   str = myFiles[0].read()
-   print "file has: ", str
+str = myFiles[0].read()
+print "file has: ", str
 #fo_1=open("file1.txt" , "wb+")
 #print "Name of the file: ", fo_1.name
 #print "closed or not : ", fo_1.closed
@@ -28,5 +35,5 @@ for x in range(3):
 
 #close the file
 #fo_1.close()
-   for j in range(3):
-      myFiles[j].close()
+for j in range(3):
+   myFiles[j].close()
