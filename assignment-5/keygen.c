44aa2c5fcd76c0468faed18898b0bffc3a1a61bc
wongnich <wongnich@10-248-112-136.wireless.oregonstate.edu>
2016-11-30 17:33:16 -0800

    finished keygen.c

--- assignment-5/keygen.c
+++ assignment-5/keygen.c
@@ -5,13 +5,19 @@
 
 
 int main(int argc, char *argv[]){
-   if(argc!=2)	exit(1);		//not enough arguments
-   
-   int num=atoi(argv[1]);
-   //testing
-   printf("rand int: %d\n", num);
+   if(argc!=2)	exit(1);		//not right amount of arguments
 
+   int length=atoi(argv[1]), i, rand;	//init all variables
+   char randomLetter;
+   char *string = malloc(sizeof(char)*length+1);
+   memset(string, '\0', length);
 
+   srand(time(NULL));
+
+   for(i=0; i<length; i++)
+	string[i]="ABCDEFGHIJKLMNOPQRSTUVWXYZ "[random () % 27];      //random char from string
+   string[length]='\n';		//add newling last
+   printf("%s", string);   //goes to stdout
 
    return 0;
 }
